4a268b5acb7ea72dde6cd906d63f83be53dbd388
santoshbohra123 <universebhaijan111@gmail.com>
2025-04-01 03:24:31 +0530

    chhwbc

--- product.js
+++ product.js
@@ -33,7 +33,7 @@ document.getElementById("order-form").addEventListener("submit", function (event
     document.getElementById("total-amount").textContent = totalAmount;
     document.getElementById("order-form").style.display = "none";
 
-    let item= document.getElementById("item-name").inputValue;
+    let item= document.getElementById("bill-item-name");
 
     if (paymentMethod === "cod") {
       document.getElementById("bill-section").style.display = "block";
